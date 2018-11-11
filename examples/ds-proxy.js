8c9065938a8300bb57c2bd8e48f31f9d74c55fa2
Cris Mihalache <me@f3rno.com>
2018-11-11 15:19:43 +0100

    (refactor) connect to DB before starting example

--- examples/ds-proxy.js
+++ examples/ds-proxy.js
@@ -9,43 +9,56 @@ const HFServer = require('../')
 const debug = require('debug')('bfx:hf:server:examples:ds-proxy')
 const WS = require('ws')
 const SocksProxyAgent = require('socks-proxy-agent')
+const { startDB, connectDB } = require('bfx-hf-models')
 
 const {
   API_KEY, API_SECRET, PORT, DS_PORT, AS_PORT, PROXY, TRANSFORM,
   SOCKS_PROXY_URL, REST_URL, WS_URL
 } = process.env
 
-const s = new HFServer({
-  apiKey: API_KEY,
-  apiSecret: API_SECRET,
-  transform: !!TRANSFORM,
-  proxy: !!PROXY,
-  asPort: AS_PORT,
-  dsPort: DS_PORT,
-  port: PORT,
-  agent: SOCKS_PROXY_URL ? new SocksProxyAgent(SOCKS_PROXY_URL) : null,
-  restURL: REST_URL,
-  wsURL: WS_URL,
-})
+const run = async () => {
+  await startDB(`${__dirname}/../db`)
+  await connectDB('hf-server')
 
-const ws = new WS(`ws://localhost:${PORT}`)
+  const s = new HFServer({
+    apiKey: API_KEY,
+    apiSecret: API_SECRET,
+    transform: !!TRANSFORM,
+    proxy: !!PROXY,
+    asPort: AS_PORT,
+    dsPort: DS_PORT,
+    port: PORT,
+    agent: SOCKS_PROXY_URL ? new SocksProxyAgent(SOCKS_PROXY_URL) : null,
+    restURL: REST_URL,
+    wsURL: WS_URL,
+  })
 
-ws.on('error', (err) => {
-  debug('socket error: %s', err.message)
-})
+  const ws = new WS(`ws://localhost:${PORT}`)
 
-ws.on('message', (msg) => {
-  debug('recv message: %s', msg)
-})
+  ws.on('error', (err) => {
+    debug('socket error: %s', err.message)
+  })
 
-ws.on('close', () => {
-  debug('socket closed')
-})
+  ws.on('message', (msg) => {
+    debug('recv message: %s', msg)
+  })
 
-ws.on('open', () => {
-  debug('socket opened')
+  ws.on('close', () => {
+    debug('socket closed')
+  })
 
-  setTimeout(() => {
-    ws.send(JSON.stringify(['ds', ['get.markets']]))
-  }, 1000)
-})
+  ws.on('open', () => {
+    debug('socket opened')
+
+    setTimeout(() => {
+      ws.send(JSON.stringify(['ds', ['get.markets']]))
+      ws.send(JSON.stringify(['as', ['get.aos']]))
+    }, 1000)
+  })
+}
+
+try {
+  run()
+} catch (e) {
+  debug('error: %s', e.message)
+}
