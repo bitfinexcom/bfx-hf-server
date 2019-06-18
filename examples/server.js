a496aced5de23f309a77b4e5455cfec2855fe5ef
Cris Mihalache <me@f3rno.com>
2019-06-19 00:18:36 +0700

    (refactor) use new HFDB system in server

--- examples/server.js
+++ examples/server.js
@@ -5,34 +5,34 @@ process.env.DEBUG = '*'
 require('dotenv').config()
 require('bfx-hf-util/lib/catch_uncaught_errors')
 
-const HFServer = require('../')
-const { startDB, connectDB } = require('bfx-hf-models')
+const HFDB = require('bfx-hf-models')
+const HFDBLowDBAdapter = require('bfx-hf-models-adapter-lowdb')
+const { schema: HFDBBitfinexSchema } = require('bfx-hf-ext-plugin-bitfinex')
 const SocksProxyAgent = require('socks-proxy-agent')
+const HFServer = require('../')
 
 const {
-  API_KEY, API_SECRET, PORT, DS_PORT, AS_PORT, PROXY, TRANSFORM,
-  SOCKS_PROXY_URL, REST_URL, WS_URL
+  API_KEY, API_SECRET, PORT, DS_PORT, AS_PORT, TRANSFORM, DB_FILENAME,
+  SOCKS_PROXY_URL, REST_URL, WS_URL,
 } = process.env
 
-const run = async () => {
-  await startDB(`${__dirname}/../db`)
-  await connectDB('hf-server')
-
-  const s = new HFServer({
-    apiKey: API_KEY,
-    apiSecret: API_SECRET,
-    transform: !!TRANSFORM,
-    asPort: AS_PORT,
-    dsPort: DS_PORT,
-    port: PORT,
-    agent: SOCKS_PROXY_URL ? new SocksProxyAgent(SOCKS_PROXY_URL) : null,
-    restURL: REST_URL,
-    wsURL: WS_URL,
+const db = new HFDB({
+  schema: HFDBBitfinexSchema,
+  adapter: HFDBLowDBAdapter({
+    dbPath: `${__dirname}/../${DB_FILENAME}`,
+    schema: HFDBBitfinexSchema
   })
-}
+})
 
-try {
-  run()
-} catch (e) {
-  debug('error: %s', e.message)
-}
+new HFServer({
+  db,
+  apiKey: API_KEY,
+  apiSecret: API_SECRET,
+  transform: !!TRANSFORM,
+  asPort: AS_PORT,
+  dsPort: DS_PORT,
+  port: PORT,
+  agent: SOCKS_PROXY_URL ? new SocksProxyAgent(SOCKS_PROXY_URL) : null,
+  restURL: REST_URL,
+  wsURL: WS_URL,
+})
