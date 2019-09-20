2d98e69823502275a21bc6772b80693e3c04f500
Cris Mihalache <me@f3rno.com>
2019-09-20 10:37:08 +0700

    (overwrite commit to fix conflicts)

--- examples/server.js
+++ examples/server.js
@@ -1,38 +1,12 @@
 'use strict'
 
-process.env.DEBUG = '*'
+process.env.DEBUG = 'bfx:hf:*'
 
-require('dotenv').config()
-require('bfx-hf-util/lib/catch_uncaught_errors')
+const startHFServer = require('..')
 
-const HFDB = require('bfx-hf-models')
-const HFDBLowDBAdapter = require('bfx-hf-models-adapter-lowdb')
-const { schema: HFDBBitfinexSchema } = require('bfx-hf-ext-plugin-bitfinex')
-const SocksProxyAgent = require('socks-proxy-agent')
-const HFServer = require('../')
-
-const {
-  API_KEY, API_SECRET, PORT, DS_PORT, AS_PORT, TRANSFORM, DB_FILENAME,
-  SOCKS_PROXY_URL, REST_URL, WS_URL,
-} = process.env
-
-const db = new HFDB({
-  schema: HFDBBitfinexSchema,
-  adapter: HFDBLowDBAdapter({
-    dbPath: `${__dirname}/../${DB_FILENAME}`,
-    schema: HFDBBitfinexSchema
-  })
-})
-
-new HFServer({
-  db,
-  apiKey: API_KEY,
-  apiSecret: API_SECRET,
-  transform: !!TRANSFORM,
-  asPort: AS_PORT,
-  dsPort: DS_PORT,
-  port: PORT,
-  agent: SOCKS_PROXY_URL ? new SocksProxyAgent(SOCKS_PROXY_URL) : null,
-  restURL: REST_URL,
-  wsURL: WS_URL,
+startHFServer({
+  uiDBPath: `${__dirname}/../db/ui.json`,
+  algoDBPath: `${__dirname}/../db/algos.json`,
+  hfBitfinexDBPath: `${__dirname}/../db/hf-bitfinex.json`,
+  hfBinanceDBPath: `${__dirname}/../db/hf-binance.json`
 })
