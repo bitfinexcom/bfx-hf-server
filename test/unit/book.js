65aac2ad147cd0b3f490ae92fea088c6f42013f1
Robert Kowalski <rok@kowalski.gd>
2020-09-30 14:35:25 +0200

    fix: do not crash with empty orderbooks
    this fixes an issue in the detection of snapshots. given the
    orderbook and snapshot was empty, the app would crash on the next
    update, i.e. an order submit

---
+++ test/unit/book.js
@@ -0,0 +1,19 @@
+/* eslint-env mocha */
+'use strict'
+
+const assert = require('assert')
+
+const updateBook = require('../../lib/exchange_clients/bitfinex/recv/data_handlers/book.js')
+
+describe('order books', () => {
+  it('does not crash: empty snapshot and update following', () => {
+    const msg1 = [12282, []]
+    const msg2 = [12295, [4000, 1, -1]]
+
+    const opts = { books: {}, lastBookPacketSent: {} }
+    updateBook(opts, msg1, { symbol: 'tBTCUSD' })
+    updateBook(opts, msg2, { symbol: 'tBTCUSD' })
+
+    assert.deepStrictEqual(opts.books, { tBTCUSD: [[4000, 1, -1]] })
+  })
+})
