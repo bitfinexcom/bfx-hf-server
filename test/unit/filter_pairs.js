d3ee90a4ea5efaea267c898d59a8f04eeefd59ce
Robert Kowalski <rok@kowalski.gd>
2020-10-13 12:09:08 +0200

    paper trading: filter pairs in menu selection
    uses the `pub:list:currency:paper` endpoint to get the current
    list of paper pairs, available at:
    
    https://api-pub.bitfinex.com/v2/conf/pub:list:currency:paper
    
    currently filtered:
    
    ```
    ["AAA","BBB","TESTBTC","TESTBTCF0","TESTUSD","TESTUSDT","TESTUSDTF0"]
    ```

---
+++ test/unit/filter_pairs.js
@@ -0,0 +1,23 @@
+/* eslint-env mocha */
+'use strict'
+
+const assert = require('assert')
+
+const filterPairs = require('../../lib/exchange_clients/bitfinex/util/filter_pairs.js')
+
+describe('filter pairs', () => {
+  it('filters out paper trading pairs', () => {
+    const exclude = [
+      'AAA', 'BBB', 'TESTBTC', 'TESTBTCF0',
+      'TESTUSD', 'TESTUSDT', 'TESTUSDTF0'
+    ]
+
+    const symbols = [
+      'AAABBB', 'TESTBTCF0:TESTUSDTF0', 'FOO:TESTBTC',
+      'BTCUSD', 'ETHUSD', 'XAUTF0:USTF0'
+    ]
+
+    const res = filterPairs(symbols, exclude)
+    assert.deepStrictEqual(res, ['BTCUSD', 'ETHUSD', 'XAUTF0:USTF0'])
+  })
+})
