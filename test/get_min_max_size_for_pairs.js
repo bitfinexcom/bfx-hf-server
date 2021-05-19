c72d69bde74ba8df85a54738d0401e4474129306
Abhishek Shrestha <avsek.stha07@gmail.com>
2021-05-19 22:16:18 +0545

    Validate orderform params (#96)
    * add min and max order size for market pairs
    * test for fetching min and max order size of market pairs
    * check for invalid orderform params and return error with the respective fields and message

---
+++ test/get_min_max_size_for_pairs.js
@@ -0,0 +1,38 @@
+/* eslint-env mocha */
+'use strict'
+
+const assert = require('assert')
+
+const getMinMaxSizeForPairs = require('../lib/exchange_clients/bitfinex/util/get_min_max_size_for_pairs.js')
+
+describe('get min and max size for all pairs', () => {
+  it('fetches the min and max size for all pairs', () => {
+    const pairConf = [
+      [
+        [
+          'ETHUSD',
+          [null, null, null, '0.006', '5000.0']
+        ],
+        [
+          'BTCUSD',
+          [null, null, null, '0.0002', '2000.0']
+        ]
+      ],
+      [
+        [
+          'BTCDOMF0:USTF0',
+          [null, null, null, '0.02', '100.0']
+        ]
+      ]
+    ]
+
+    const res = getMinMaxSizeForPairs(pairConf)
+    assert.deepStrictEqual(res.size, 3)
+    assert.deepStrictEqual(res.get('ETHUSD').minSize, 0.006)
+    assert.deepStrictEqual(res.get('ETHUSD').maxSize, 5000)
+    assert.deepStrictEqual(res.get('BTCUSD').minSize, 0.0002)
+    assert.deepStrictEqual(res.get('BTCUSD').maxSize, 2000)
+    assert.deepStrictEqual(res.get('BTCDOMF0:USTF0').minSize, 0.02)
+    assert.deepStrictEqual(res.get('BTCDOMF0:USTF0').maxSize, 100)
+  })
+})
