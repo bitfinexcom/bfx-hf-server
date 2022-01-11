b8b5fe47d8fb74fd2c0f8d1e075fc919b6950ab0
Tarcisio Ferraz <tar-2008.ferraz@hotmail.com>
2022-01-11 14:42:43 -0300

    centralize user settings

---
+++ test/unit/lib/util/user_settings.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-env mocha */
+'use strict'
+
+const { stub } = require('sinon')
+const { expect } = require('chai')
+const { UserSettings: { _default } } = require('bfx-hf-ui-config')
+
+const getUserSettings = require('util/user_settings')
+
+describe('get user settings', () => {
+  const getAll = stub()
+  const UserSettings = { getAll }
+  const db = { UserSettings }
+
+  it('user does not have saved settings', async () => {
+    getAll.resolves({})
+
+    const result = await getUserSettings(db)
+
+    expect(result).to.eql({
+      ..._default,
+      dms: true
+    })
+  })
+
+  it('user has saved settings', async () => {
+    const savedSettings = {
+      custom: 'value',
+      dms: false
+    }
+    getAll.resolves({ userSettings: savedSettings })
+
+    const result = await getUserSettings(db)
+
+    expect(result).to.eql({
+      ..._default,
+      ...savedSettings
+    })
+  })
+})
