b949c2f667e528b0607d4454de24c5ce9ddb5f70
Tarcísio Zotelli Ferraz <tar-2008.ferraz@hotmail.com>
2022-11-18 08:19:31 -0300

    AOs per mode (#194)

--- test/unit/lib/ws_servers/api/algos/algo_worker.js
+++ test/unit/lib/ws_servers/api/algos/algo_worker.js
@@ -2,6 +2,7 @@
 /* eslint-env mocha */
 'use strict'
 
+const _omit = require('lodash/omit')
 const { assert, createSandbox } = require('sinon')
 const { expect } = require('chai')
 const proxyquire = require('proxyquire').noCallThru()
@@ -42,18 +43,20 @@ describe('AlgoWorker', () => {
     dir: '/trace-dir'
   }
 
+  const mode = 'main'
   const settings = {
     dms: 'dms',
     affiliateCode: 'affiliate code',
     wsURL: 'ws url',
     restURL: 'rest url',
-    signalTracerOpts
+    signalTracerOpts,
+    mode
   }
   const algoOrders = []
   const bcast = WsStub
-  const algoDB = { AlgoOrder: { set: sandbox.stub() } }
+  const algoDB = { AlgoOrder: { find: sandbox.stub(), set: sandbox.stub() } }
   const logAlgoOpts = null
-  const marketData = new Map()
+  const marketData = new Map([['tBTCUSD', { wsID: 'tBTCUSD', p: 0 }]])
   const apiKey = 'api key'
   const apiSecret = 'api secret'
   const authToken = 'auth token'
@@ -81,23 +84,21 @@ describe('AlgoWorker', () => {
     }
 
     it('should create the host and register the events', async () => {
-      const aoInstance = {
-        state: {
-          active: true,
-          createdAt: 1633936704466,
-          id: 'id',
-          gid: 'gid',
+      const aoDetails = {
+        gid: 'gid',
+        algoID: 'algo id',
+        createdAt: 1633936704466,
+        state: JSON.stringify({
           name: 'name',
-          args: 'args',
+          args: { symbol: 'tBTCUSD' },
           label: 'label'
-        }
+        })
       }
-      AOHostStub.getAOInstances.returns([aoInstance])
       AOHostStub.connect.resolves(authResponse)
+      algoDB.AlgoOrder.find.resolves([aoDetails])
 
-      const algoWorker = new AlgoWorker(settings, algoOrders, bcast, algoDB, logAlgoOpts, marketData)
+      const algoWorker = new AlgoWorker(settings, algoOrders, bcast, algoDB, logAlgoOpts, marketData, mode)
       expect(algoWorker.isStarted).to.be.false
-
       await algoWorker.start({ apiKey, apiSecret, authToken, userId })
 
       // generate auth token with api credentials
@@ -126,15 +127,11 @@ describe('AlgoWorker', () => {
       assert.calledWithExactly(AOHostStub.connect)
       // publish opened event
       assert.calledWithExactly(WsStub.firstCall, ['opened', userId, 'bitfinex'])
-      // send active instances
-      assert.calledWithExactly(WsStub.secondCall, ['data.aos', 'bitfinex', [[
-        aoInstance.state.id,
-        aoInstance.state.gid,
-        aoInstance.state.name,
-        aoInstance.state.label,
-        aoInstance.state.args,
-        aoInstance.state.createdAt
-      ]]])
+      // send active aos
+      assert.calledWithExactly(WsStub.secondCall, ['data.aos', 'bitfinex', mode, [{
+        ..._omit(aoDetails, 'state'),
+        ...JSON.parse(aoDetails.state)
+      }]])
       // final worker inner-state
       expect(algoWorker.userId).to.be.eq(userId)
       expect(algoWorker.isStarted).to.be.true
@@ -254,7 +251,7 @@ describe('AlgoWorker', () => {
           'Started AO name on Bitfinex',
           { key: 'startedAO', props: { name: 'name', target: 'Bitfinex' } }
         ])
-        assert.calledWithExactly(WsStub.secondCall, ['data.ao', 'bitfinex', { ...uiData }])
+        assert.calledWithExactly(WsStub.secondCall, ['data.ao', 'bitfinex', mode, { ...uiData }])
         expect(returnedGid).to.eq(gid)
       })
     })
@@ -282,7 +279,7 @@ describe('AlgoWorker', () => {
           'Started AO name on Bitfinex',
           { key: 'startedAO', props: { name: 'name', target: 'Bitfinex' } }
         ])
-        assert.calledWithExactly(WsStub.secondCall, ['data.ao', 'bitfinex', { ...uiData }])
+        assert.calledWithExactly(WsStub.secondCall, ['data.ao', 'bitfinex', mode, { ...uiData }])
         expect(returnedGid).to.eq(gid)
       })
     })
