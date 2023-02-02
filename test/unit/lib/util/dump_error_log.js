4f256b28f8a4abd1469fa3242dadc7ae0c5e1010
Md. Ariful Islam <arifjmamun24@gmail.com>
2023-02-02 19:17:17 +0600

    use fs/promises

--- test/unit/lib/util/dump_error_log.js
+++ test/unit/lib/util/dump_error_log.js
@@ -2,10 +2,11 @@
 /* eslint-env mocha */
 'use strict'
 
+const chai = require('chai')
 const sinon = require('sinon')
-const { expect } = require('chai')
 const proxyquire = require('proxyquire')
 const fs = require('fs')
+const { expect } = chai
 
 describe('dump error log', () => {
   let mkdirStub
@@ -15,18 +16,17 @@ describe('dump error log', () => {
   let dumpErrorLog
 
   beforeEach(() => {
-    mkdirStub = sinon.stub(fs, 'mkdir').callsFake((path, callback) => {
-      callback(null)
-    })
-    appendFileStub = sinon.stub(fs, 'appendFile').callsFake((path, data, callback) => {
-      callback(null)
-    })
-    existsSyncStub = sinon.stub(fs, 'existsSync').returns(true)
+    mkdirStub = sinon.stub(fs.promises, 'mkdir')
+    appendFileStub = sinon.stub(fs.promises, 'appendFile')
+    existsSyncStub = sinon.stub(fs, 'existsSync')
     sendStub = sinon.stub()
+
     dumpErrorLog = proxyquire('../../../../lib/util/dump_error_log', {
-      fs: {
+      'fs/promises': {
         mkdir: mkdirStub,
-        appendFile: appendFileStub,
+        appendFile: appendFileStub
+      },
+      fs: {
         existsSync: existsSyncStub
       },
       './ws/send': sendStub
@@ -34,53 +34,44 @@ describe('dump error log', () => {
   })
 
   afterEach(() => {
-    existsSyncStub.restore()
     mkdirStub.restore()
     appendFileStub.restore()
+    existsSyncStub.restore()
+    sendStub.reset()
   })
 
   it('should create the log directory if it does not exist', async () => {
-    const ws = {}
-    const errorLog = JSON.stringify({ error: 'test error' })
-    existsSyncStub.restore()
-    existsSyncStub = sinon.stub(fs, 'existsSync').returns(false)
-    dumpErrorLog = proxyquire('../../../../lib/util/dump_error_log', {
-      fs: {
-        mkdir: mkdirStub,
-        appendFile: appendFileStub,
-        existsSync: existsSyncStub
-      },
-      './ws/send': sendStub
-    })
-    await dumpErrorLog(ws, errorLog)
+    existsSyncStub.returns(false)
+
+    await dumpErrorLog({}, {})
+
     expect(mkdirStub.calledOnce).to.be.true
   })
 
+  it('should not create the log directory if it is already exist', async () => {
+    existsSyncStub.returns(true)
+
+    await dumpErrorLog({}, {})
+
+    expect(mkdirStub.notCalled).to.be.true
+  })
+
   it('should append the error log to the app.log file', async () => {
-    const ws = {}
-    const errorLog = JSON.stringify({ error: 'test error' })
-    await dumpErrorLog(ws, errorLog)
+    existsSyncStub.returns(true)
+
+    const errorLog = JSON.stringify({ error: 'app error' })
+    await dumpErrorLog({}, errorLog)
+
     expect(appendFileStub.calledOnce).to.be.true
     expect(appendFileStub.firstCall.args[1]).to.contain(errorLog)
   })
 
-  it('should send a message to the WebSocket object if an error occurs', async () => {
-    const ws = {}
-    const errorLog = JSON.stringify({ error: 'test error' })
-    appendFileStub.restore()
-    appendFileStub = sinon.stub(fs, 'appendFile').callsFake((path, data, callback) => {
-      callback(new Error('Test error'))
-    })
-    dumpErrorLog = proxyquire('../../../../lib/util/dump_error_log', {
-      fs: {
-        mkdir: mkdirStub,
-        appendFile: appendFileStub,
-        existsSync: existsSyncStub
-      },
-      './ws/send': sendStub
-    })
-    await dumpErrorLog(ws, errorLog)
-    expect(sendStub.calledOnce).to.be.true
-    expect(sendStub.firstCall.args[1][1]).to.equal('Test error')
+  it('should send a ws message if an error occurs', async () => {
+    existsSyncStub.returns(false)
+    mkdirStub.rejects(new Error('MKDIR_ERROR'))
+
+    await dumpErrorLog({}, {})
+
+    expect(sendStub.calledWith({}, ['data.app_log_error', 'MKDIR_ERROR'])).to.be.true
   })
 })
